45e76b8eb92644692828de64e7fa5ead75b468f3
seema987 <seemasitaula987@email.com>
2023-04-21 09:52:08 +0930

    TICTACTOE

---
+++ function.js
@@ -0,0 +1,104 @@
+var playerOne; 
+var playerTwo;
+var section = document.querySelector('section')
+var left1 = document.querySelector('section .left1')
+var left2 =  document.querySelector('section .left2')
+var left3 = document.querySelector ('section .left3')
+var middle1 = document.querySelector('section .middle1')
+var middle2 = document.querySelector('section .middle2')
+var middle3 = document.querySelector('section .middle3')
+var right1 = document.querySelector ('section .right1')
+var right2 = document.querySelector ('section .right2')
+var right3 = document.querySelector ('section .right3')
+var main = document.querySelectorAll('main')
+var cross = document.querySelector('.cross')
+var circle = document.querySelector('.circle')
+
+
+function boxText(X, Y) {
+    right1.addEventListener(boxText('X', 'Y'))
+    right1.textContent = "X"
+    
+}
+
+// if (playerOne === cross) {
+// section1.addEventListener('click', function(event){
+//     var choices = event.target 
+//     if (choices.tagName === 'ASIDE' ){
+//         section2.classList.toggle('textSize1')
+//         choices.textContent = 'X' 
+//     }
+
+// })
+
+
+// section2.addEventListener('click', function(event){
+//     var choices = event.target 
+//     if (choices.tagName === 'ASIDE' ){
+//         section2.classList.toggle('textSize2')
+//         choices.textContent = 'X'
+        
+//     }
+// })
+
+
+// section3.addEventListener('click', function(event){
+//     var choices = event.target 
+//     if (choices.tagName === 'ASIDE' ){
+//         section2.classList.toggle('textSize3')
+//         choices.textContent = 'X'
+        
+//     }
+// })
+
+
+
+    
+
+
+
+// var userChoice = [img1, img2]
+ 
+// if (playerChoice === img1) {
+//     document.left1.appendChild(img1);
+// }
+
+
+
+// var left1 = document.querySelector('.left1')
+// left1.addEventListener('click', function(){
+//     var img1 = document.querySelector('.img1')
+//     img1.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/U%2B2179.svg/1200px-U%2B2179.svg.png"  
+//     img1.style.border = '2px'
+//     img1.style.width = '60px'
+//     img1.style.height = '60px'
+//     img1.style.marginLeft = '20px'
+//     img1.style.marginTop = '10px'
+//     document.left1.appendChild(img1);
+
+    
+//     var img2 = document.querySelector('.img2')
+//     img2.src = "https://images.unsplash.com/photo-1613905392914-2ca5fbab0923?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fG98ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+//     img2.style.border = '2px'
+//     img2.style.width = '60px'
+//     img2.style.height = '60px'
+//     img2.style.marginLeft = '20px'
+//     img2.style.marginTop = '10px'
+//     document.left1.appendChild(img2);
+
+//     left1.toggle(img2)
+    
+// })
+
+// var img1 = document.querySelector('.img1')
+
+// img1.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/U%2B2179.svg/1200px-U%2B2179.svg.png" 
+// img1.style.border = '2px'
+//     img1.style.width = '60px'
+//     img1.style.height = '60px'
+//     img1.style.marginLeft = '20px'
+//     img1.style.marginTop = '10px' 
+// var img2 = document.querySelector('.img2')
+// img2.src = "https://images.unsplash.com/photo-1613905392914-2ca5fbab0923?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fG98ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+
+
